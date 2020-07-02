c4fc11984f288f44f2544d3a264e5837992bbfbd
zimbardoex <zimbardoex@gmail.com>
2020-07-02 21:05:28 +1200

    Add basic listing component

--- vue-auction/src/App.vue
+++ vue-auction/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Listing name="Lenovo T480" description="It's very good" price="450" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Listing from './components/Listing.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Listing,
   }
 }
 </script>
---
+++ vue-auction/src/components/Listing.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ name }}</h1>
+    <p>
+        {{ description }}
+    </p>
+    <p>
+     {{ `Price: $${price}` }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Listing',
+  props: {
+    name: String,
+    price: String,
+    description: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
