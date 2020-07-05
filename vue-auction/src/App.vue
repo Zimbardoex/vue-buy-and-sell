22f5ea9235f2ad96e3eb430605715e0588b733a7
zimbardoex <zimbardoex@gmail.com>
2020-07-05 17:22:41 +1200

    Fetch and display listings from api

--- vue-auction/src/App.vue
+++ vue-auction/src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Listing name="Lenovo T480" description="It's very good" price="450" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Listing
+      v-for="listing in listings"
+      v-bind:key="listing.id"
+      v-bind:name="listing.name"
+      v-bind:description="listing.description"
+      v-bind:price="listing.price"
+    />
   </div>
 </template>
 
 <script>
-import Listing from './components/Listing.vue'
+import Listing from "./components/Listing.vue";
+
+const API_URL = "http://localhost:5000/api";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Listing,
   },
-}
+  mounted() {
+    fetch(`${API_URL}/listings`)
+      .then((response) => response.json())
+      .then((result) => {
+        this.listings = result;
+      });
+  },
+  data: () => ({
+    listings: [],
+  }),
+};
 </script>
 
 <style>
