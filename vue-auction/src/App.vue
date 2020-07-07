b19e97815255fb9e88b9869d451ccba5f24d1f7d
zimbardoex <zimbardoex@gmail.com>
2020-07-07 14:38:35 +1200

    Implement view router

--- vue-auction/src/App.vue
+++ vue-auction/src/App.vue
@@ -1,36 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Listings
-      v-for="listing in listings"
-      v-bind:key="listing.id"
-      v-bind:name="listing.name"
-      v-bind:description="listing.description"
-      v-bind:price="listing.price"
-    />
+    <p>
+      <router-link to="/listings">Go to Listings</router-link>
+    </p>
+    <!-- component matched by the route will render here -->
+    <router-view />
   </div>
 </template>
 
 <script>
-import Listings from "./components/Listings.vue";
-
-const API_URL = "http://localhost:5000/api";
-
 export default {
   name: "App",
-  components: {
-    Listings,
-  },
-  mounted() {
-    fetch(`${API_URL}/listings`)
-      .then((response) => response.json())
-      .then((result) => {
-        this.listings = result;
-      });
-  },
-  data: () => ({
-    listings: [],
-  }),
 };
 </script>
 
