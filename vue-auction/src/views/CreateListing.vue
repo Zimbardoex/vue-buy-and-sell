81252c08e2e89a00216680f2464a5ce859abc518
zimbardoex <zimbardoex@gmail.com>
2020-07-10 17:08:46 +1200

    Update listing creation form

--- vue-auction/src/views/CreateListing.vue
+++ vue-auction/src/views/CreateListing.vue
@@ -1,33 +1,53 @@
 <template>
-  <form @submit.prevent="createListing" @keydown.prevent.enter>
-    <label for="name">Title</label>
-    <input v-model="listing.name" type="text" name="name" />
-    <br />
-    <label for="description">Description</label>
-    <textarea
-      v-model="listing.description"
-      type="textarea"
-      name="description"
-    />
-    <br />
-    <label for="price">Price</label>
-    <input v-model.number="listing.price" type="number" step="0.01" name="price" />
-    <br />
-    <select v-model="listing.category">
-      <option disabled value="">Please select one</option>
-      <option>Books</option>
-      <option>Cars</option>
-      <option>Computers</option>
-    </select>
-    <br />
-    <button type="submit">Submit</button>
-    <p v-if="errors">
-      <b>Please correct the following error(s):</b>
-      <ul>
-        <li v-for="errorList in errors" :key="errorList[0]">{{ errorList[0] }}</li>
-      </ul>
-    </p>
-  </form>
+  <div class="card">
+    <form @submit.prevent="createListing" @keydown.prevent.enter class="form">
+      <div class="formElement">
+        <b-field label="Title" message="">
+          <b-input v-model="listing.name"></b-input>
+        </b-field>
+      </div>
+
+      <div class="formElement">
+        <b-field label="Description">
+          <b-input type="textarea" v-model="listing.description"></b-input>
+        </b-field>
+      </div>
+
+      <div class="formElement">
+        <b-field label="Price">
+          <b-numberinput step="0.01" v-model="listing.price" />
+        </b-field>
+      </div>
+
+      <div class="formElement">
+        <b-field label="Category" v-model="listing.category">
+          <b-select placeholder="Select a category" icon="account">
+            <optgroup label="Computers">
+              <option value="laptop">Laptops</option>
+              <option value="desktops">Desktops</option>
+            </optgroup>
+
+            <optgroup label="Motors">
+              <option value="cars">Cars</option>
+              <option value="boats">Boats</option>
+              <option value="motorbikes">Motorbikes</option>
+            </optgroup>
+          </b-select>
+        </b-field>
+      </div>
+      <div class="formElement">
+        <b-button type="is-primary" native-type="submit">Submit</b-button>
+      </div>
+      <div class="formElement">
+        <p v-if="errors">
+          <b>Please correct the following error(s):</b>
+          <ul>
+            <li v-for="errorList in errors" :key="errorList[0]">{{ errorList[0] }}</li>
+          </ul>
+        </p>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -55,10 +75,10 @@ export default {
       })
         .then((response) => response.json())
         .then((result) => {
-          if (result.errors){
-            this.errors = result.errors
+          if (result.errors) {
+            this.errors = result.errors;
           } else {
-             this.$router.push({ path: "listings"});
+            this.$router.push({ path: "listings" });
           }
         });
     },
@@ -66,4 +86,24 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.form {
+  display: flex;
+  /* align-items: flex-start; */
+  justify-content: center;
+  flex-direction: column;
+  padding: 1rem;
+}
+.card {
+  max-width: 35vw;
+  min-width: 350px;
+  margin: 0 auto;
+}
+.formElement {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  text-align: start;
+  margin: 0.5rem;
+}
+</style>
