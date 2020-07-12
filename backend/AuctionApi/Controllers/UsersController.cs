b389350a789d9d2659ec251da8950a89d774ae6e
zimbardoex <zimbardoex@gmail.com>
2020-07-12 21:34:46 +1200

    Add login route to user controller

--- backend/AuctionApi/Controllers/UsersController.cs
+++ backend/AuctionApi/Controllers/UsersController.cs
@@ -5,76 +5,95 @@ using System.Collections.Generic;
 
 namespace UsersApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UsersController : ControllerBase
+  {
+    private readonly UserService _userService;
+
+    public UsersController(UserService userService)
     {
-        private readonly UserService _userService;
-
-        public UsersController(UserService userService)
-        {
-            _userService = userService;
-        }
-
-        [HttpGet]
-        public ActionResult<List<User>> Get() =>
-            _userService.Get();
-
-        [HttpGet("{id:length(24)}", Name = "GetUser")]
-        public ActionResult<User> Get(string id)
-        {
-            var user = _userService.Get(id);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return user;
-        }
-
-        [HttpPost]
-        public ActionResult<User> Create(User user)
-        {
-            string saltyString = System.Environment.GetEnvironmentVariable("SALTY_STRING");
-            string password = user.Password + saltyString;
-            string salt = BCrypt.Net.BCrypt.GenerateSalt();
-            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
-
-            user.Password = hashedPassword;
-            _userService.Create(user);
-
-            return CreatedAtRoute("GetUser", new { id = user.Id.ToString() }, user);
-        }
-
-        [HttpPut("{id:length(24)}")]
-        public IActionResult Update(string id, User userIn)
-        {
-            var user = _userService.Get(id);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            _userService.Update(id, userIn);
-
-            return NoContent();
-        }
-
-        [HttpDelete("{id:length(24)}")]
-        public IActionResult Delete(string id)
-        {
-            var user = _userService.Get(id);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            _userService.Remove(user.Id);
-
-            return NoContent();
-        }
+      _userService = userService;
     }
+
+    [HttpGet]
+    public ActionResult<List<User>> Get() =>
+        _userService.Get();
+
+    [HttpGet("{id:length(24)}", Name = "GetUser")]
+    public ActionResult<User> Get(string id)
+    {
+      var user = _userService.Get(id);
+
+      if (user == null)
+      {
+        return NotFound();
+      }
+
+      return user;
+    }
+
+
+    [HttpPost]
+    [Route("login")]
+    public ActionResult<User> Login(UserLogin userToAuthenticate)
+    {
+      List<User> users = _userService.Get();
+      User user = users.Find( user => user.Email == userToAuthenticate.Email);
+
+      string password = userToAuthenticate.Password;
+      string hashedPassword = user.Password;
+      string saltyString = System.Environment.GetEnvironmentVariable("SALTY_STRING");
+
+      if (BCrypt.Net.BCrypt.Verify(password + saltyString, hashedPassword)) {
+        return Ok(user);
+      } else {
+        return StatusCode(401, "Nope");
+      }
+    }
+
+    [HttpPost]
+    public ActionResult<User> Create(User user)
+    {
+      string saltyString = System.Environment.GetEnvironmentVariable("SALTY_STRING");
+      string password = user.Password + saltyString;
+      string salt = BCrypt.Net.BCrypt.GenerateSalt();
+      string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
+
+      user.Password = hashedPassword;
+      _userService.Create(user);
+
+      return CreatedAtRoute("GetUser", new { id = user.Id.ToString() }, user);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public IActionResult Update(string id, User userIn)
+    {
+      var user = _userService.Get(id);
+
+      if (user == null)
+      {
+        return NotFound();
+      }
+
+      _userService.Update(id, userIn);
+
+      return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public IActionResult Delete(string id)
+    {
+      var user = _userService.Get(id);
+
+      if (user == null)
+      {
+        return NotFound();
+      }
+
+      _userService.Remove(user.Id);
+
+      return NoContent();
+    }
+  }
 }
