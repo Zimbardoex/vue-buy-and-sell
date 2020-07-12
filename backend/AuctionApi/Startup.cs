8db2e8f32a267f01c534b0f853373c584919c82f
zimbardoex <zimbardoex@gmail.com>
2020-07-12 17:55:02 +1200

    Read from .env file before startup

--- backend/AuctionApi/Startup.cs
+++ backend/AuctionApi/Startup.cs
@@ -55,6 +55,7 @@ namespace AuctionApi
 
             services.AddCors();
             services.AddControllers();
+            DotNetEnv.Env.Load();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
