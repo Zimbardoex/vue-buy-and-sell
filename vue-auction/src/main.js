2347a971a668b225db83b58ab284ad921df2abd5
zimbardoex <zimbardoex@gmail.com>
2020-07-14 16:47:58 +1200

    Add navigation guards to create a listing, signup and login routes

--- vue-auction/src/main.js
+++ vue-auction/src/main.js
@@ -7,22 +7,55 @@ import Signup from "./views/Signup";
 import Login from "./views/Login";
 import CreateListing from "./views/CreateListing";
 import VueRouter from "vue-router";
-import Buefy from 'buefy';
-import 'buefy/dist/buefy.css';
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 
 Vue.use(VueRouter);
 
-Vue.use(Buefy)
+Vue.use(Buefy);
 
 Vue.config.productionTip = false;
+
+const notLoggedInRedirect = (_0, _1, next) => {
+  if (localStorage.token) {
+    next();
+  } else {
+    next("/login");
+  }
+};
+
+const loggedInRedirect = (_0, _1, next) => {
+  if (localStorage.token) {
+    console.log('"is token"', "is token");
+    next("/");
+  } else {
+    console.log('"is not token"', "is not token");
+    next();
+  }
+};
 
 const routes = [
   { path: "/", component: Home },
   { path: "/listings", component: Listings },
   { path: "/listing", component: Listing, name: "listing" },
-  { path: "/create-listing", component: CreateListing, name: "create-listing" },
-  { path: "/signup", component: Signup, name: "signup" },
-  { path: "/login", component: Login, name: "login" },
+  {
+    path: "/create-listing",
+    component: CreateListing,
+    name: "create-listing",
+    beforeEnter: notLoggedInRedirect,
+  },
+  {
+    path: "/signup",
+    component: Signup,
+    name: "signup",
+    beforeEnter: loggedInRedirect,
+  },
+  {
+    path: "/login",
+    component: Login,
+    name: "login",
+    beforeEnter: loggedInRedirect,
+  },
 ];
 
 const router = new VueRouter({
