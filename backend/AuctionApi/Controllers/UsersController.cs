0d6f6ef09509a9ecd853931f879ffd0c1fb63291
zimbardoex <zimbardoex@gmail.com>
2020-07-14 21:51:55 +1200

    Check if email taken when creating a new user

--- backend/AuctionApi/Controllers/UsersController.cs
+++ backend/AuctionApi/Controllers/UsersController.cs
@@ -38,41 +38,43 @@ namespace UsersApi.Controllers
     [Route("login")]
     public ActionResult<LoginResponse> Login(UserLogin userToAuthenticate)
     {
-      List<User> users = _userService.Get();
-      User user = users.Find(user => user.Email == userToAuthenticate.Email);
+      User user = _userService.getUserWithEmail(userToAuthenticate.Email);
 
-      if (user != null){
+      if (user != null)
+      {
         string password = userToAuthenticate.Password;
         string hashedPassword = user.Password;
         string saltyString = System.Environment.GetEnvironmentVariable("SALTY_STRING");
         if (BCrypt.Net.BCrypt.Verify(password + saltyString, hashedPassword))
         {
           string token = _userService.generateJwt(user);
-          return Ok(new { user, token});
+          return Ok(new { user, token });
         }
         else
         {
           return StatusCode(401);
         }
-      } else {
-        return StatusCode(401);
       }
+      return StatusCode(401);
     }
 
     [HttpPost]
     public ActionResult<User> Create(User user)
     {
-      string saltyString = System.Environment.GetEnvironmentVariable("SALTY_STRING");
-      string password = user.Password + saltyString;
-      string salt = BCrypt.Net.BCrypt.GenerateSalt();
-      string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
+      if(_userService.getUserWithEmail(user.Email) == null){
+        string saltyString = System.Environment.GetEnvironmentVariable("SALTY_STRING");
+        string password = user.Password + saltyString;
+        string salt = BCrypt.Net.BCrypt.GenerateSalt();
+        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
 
-      user.Password = hashedPassword;
-      _userService.Create(user);
+        user.Password = hashedPassword;
+        _userService.Create(user);
 
-      string token = _userService.generateJwt(user);
+        string token = _userService.generateJwt(user);
 
-      return CreatedAtRoute("GetUser", new { id = user.Id.ToString()}, new {id = user.Id, email = user.Email, token} );
+        return CreatedAtRoute("GetUser", new { id = user.Id.ToString() }, new { id = user.Id, email = user.Email, token });
+      }
+      return StatusCode(409, "User with this email already exists");
     }
 
     [HttpPut("{id:length(24)}")]
--- backend/AuctionApi/Services/UserService.cs
+++ backend/AuctionApi/Services/UserService.cs
@@ -13,57 +13,64 @@ using System.Text;
 
 namespace AuctionApi.Services
 {
-    public class UserService
+  public class UserService
+  {
+    private readonly IMongoCollection<User> _users;
+
+    public UserService(IAuctionDatabaseSettings settings)
     {
-        private readonly IMongoCollection<User> _users;
+      var client = new MongoClient(settings.ConnectionString);
+      var database = client.GetDatabase(settings.DatabaseName);
 
-        public UserService(IAuctionDatabaseSettings settings)
-        {
-            var client = new MongoClient(settings.ConnectionString);
-            var database = client.GetDatabase(settings.DatabaseName);
-
-            _users = database.GetCollection<User>(settings.UsersCollectionName);
-        }
-
-        public List<User> Get() =>
-            _users.Find(user => true).ToList();
-
-        public User Get(string id) =>
-            _users.Find<User>(user => user.Id == id).FirstOrDefault();
-
-        public User Create(User user)
-        {
-            _users.InsertOne(user);
-            return user;
-        }
-
-        public void Update(string id, User userIn) =>
-            _users.ReplaceOne(user => user.Id == id, userIn);
-
-        public void Remove(User userIn) =>
-            _users.DeleteOne(user => user.Id == userIn.Id);
-
-        public void Remove(string id) =>
-            _users.DeleteOne(user => user.Id == id);
-
-        public string generateJwt(User user)
-        {
-            // generate token that is valid for 1 day
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(System.Environment.GetEnvironmentVariable("TOKEN_KEY"));
-            var issuer = Environment.GetEnvironmentVariable("TOKEN_ISSUER");
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, user.Id.ToString())
-                }),
-                Expires = DateTime.UtcNow.AddDays(1),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
-                Issuer = issuer
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            return tokenHandler.WriteToken(token);
-        }
+      _users = database.GetCollection<User>(settings.UsersCollectionName);
     }
+
+    public List<User> Get() =>
+        _users.Find(user => true).ToList();
+
+    public User Get(string id) =>
+        _users.Find<User>(user => user.Id == id).FirstOrDefault();
+
+    public User Create(User user)
+    {
+      _users.InsertOne(user);
+      return user;
+    }
+
+    public void Update(string id, User userIn) =>
+        _users.ReplaceOne(user => user.Id == id, userIn);
+
+    public void Remove(User userIn) =>
+        _users.DeleteOne(user => user.Id == userIn.Id);
+
+    public void Remove(string id) =>
+        _users.DeleteOne(user => user.Id == id);
+
+    public string generateJwt(User user)
+    {
+      // generate token that is valid for 1 day
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var key = Encoding.ASCII.GetBytes(System.Environment.GetEnvironmentVariable("TOKEN_KEY"));
+      var issuer = Environment.GetEnvironmentVariable("TOKEN_ISSUER");
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(new Claim[]
+          {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+          }),
+        Expires = DateTime.UtcNow.AddDays(1),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+        Issuer = issuer
+      };
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      return tokenHandler.WriteToken(token);
+    }
+
+    public User getUserWithEmail(String email)
+    {
+      List<User> users = Get();
+      User user = users.Find(user => user.Email == email);
+      return user;
+    }
+  }
 }
