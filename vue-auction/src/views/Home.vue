3eb714015b2108324c23721a77b3a9a93fec9678
zimbardoex <zimbardoex@gmail.com>
2020-07-15 16:26:58 +1200

    Add logout button to home

--- vue-auction/src/views/Home.vue
+++ vue-auction/src/views/Home.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <p>Welcome home</p>
+    <b-button v-if="token" type="is-primary" outlined @click="logout">
+      Logout
+    </b-button>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data: () => ({
+    token: localStorage.token,
+  }),
+  methods: {
+    logout() {
+      localStorage.removeItem("token");
+      this.$router.push("/login");
+    },
+  },
 };
 </script>
