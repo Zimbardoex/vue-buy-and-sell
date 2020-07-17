aab7245f3acb88bbe1cdb181c3bd9dd16f6b149d
zimbardoex <zimbardoex@gmail.com>
2020-07-17 16:16:45 +1200

    Remove logout button from home view

--- vue-auction/src/views/Home.vue
+++ vue-auction/src/views/Home.vue
@@ -1,17 +1,11 @@
 <template>
   <div>
     <p>Welcome home</p>
-    <b-button v-if="token" type="is-primary" outlined @click="logout">
-      Logout
-    </b-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
-  data: () => ({
-    token: localStorage.token,
-  }),
+  name: "Home"
 };
 </script>
