16debee015e9e1874407cba0c829c76d362e3107
zimbardoex <zimbardoex@gmail.com>
2020-07-17 17:13:22 +1200

    Add my listings component

---
+++ vue-auction/src/views/MyListings.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="listings">
+    <Listing
+      v-for="listing in listings"
+      v-bind:key="listing.id"
+      v-bind:id="listing.id"
+      v-bind:name="listing.name"
+      v-bind:description="listing.description"
+      v-bind:price="listing.price"
+      v-bind:image="listing.image"
+    />
+  </div>
+</template>
+
+<script>
+const API_URL = "http://localhost:5000/api";
+import Listing from "../components/Listing";
+
+export default {
+  name: "MyListings",
+  components: {
+    Listing,
+  },
+  data: () => ({
+    listings: [],
+    user: this.$store.state.user,
+  }),
+  mounted() {
+    fetch(`${API_URL}/listings/${this.user.id}`)
+      .then((response) => response.json())
+      .then((result) => {
+        this.listings = result.reverse();
+      });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.listings {
+  display: flex;
+  justify-content: center;
+  flex-flow: column wrap;
+  align-items: center;
+}
+</style>
