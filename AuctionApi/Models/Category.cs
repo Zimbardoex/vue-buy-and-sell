f0d37ecf0eccf4dd18f87a11b1039ca1b57037e9
zimbardoex <zimbardoex@gmail.com>
2020-07-19 11:31:52 +1200

    Add category model

---
+++ AuctionApi/Models/Category.cs
@@ -0,0 +1,21 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuctionApi.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        [Required]
+        [StringLength(64, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 64 characters in length")]
+        public string Name { get; set; }
+
+        [Required]
+        public string CategoryId { get; set; }
+     }
+}
