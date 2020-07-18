0dd0bf6b16fc3be86cf2500cf26ef0eedb44457f
zimbardoex <zimbardoex@gmail.com>
2020-07-19 10:21:18 +1200

    Get and display listings from api

--- vue-auction/src/components/ListingsCarousel.vue
+++ vue-auction/src/components/ListingsCarousel.vue
@@ -2,7 +2,7 @@
   <section>
     <b-carousel-list
       v-model="values"
-      :data="items"
+      :data="listings"
       :arrow-hover="arrowHover"
       :items-to-list="perList"
       :has-drag="drag"
@@ -11,17 +11,17 @@
         <div class="card">
           <div class="card-image">
             <figure class="image is-5by4">
-              <a @click="info(props.index)"><img :src="props.list.image"/></a>
+              <a @click="info(props.index)"><img :src="getImage(props.list.image)"/></a>
             </figure>
             <b-tag type="is-info" rounded style="position: absolute; top: 0;">
               <p class="control" style="margin-left: auto">
                 {{ `$ ${props.list.price}` }}
-              </p></b-tag
-            >
+              </p>
+            </b-tag>
           </div>
           <div class="card-content">
             <div class="content">
-              <p class="title is-6">{{ props.list.title }}</p>
+              <p class="title is-6">{{ props.list.name }}</p>
               <p class="subtitle is-7">Caption will go here</p>
             </div>
           </div>
@@ -32,6 +32,9 @@
 </template>
 
 <script>
+const API_URL = "http://localhost:5000/api";
+import DefaultImage from "../assets/placeholder.jpg";
+
 export default {
   name: "ListingsCarousel",
   data() {
@@ -40,41 +43,20 @@ export default {
       drag: true,
       values: 1,
       perList: 1,
-      items: [
-        {
-          title: "Slide 1",
-          image: "https://picsum.photos/id/0/1230/500",
-        },
-        {
-          title: "Slide 2",
-          image: "https://picsum.photos/id/1/1230/500",
-        },
-        {
-          title: "Slide 3",
-          image: "https://picsum.photos/id/2/1230/500",
-        },
-        {
-          title: "Slide 4",
-          image: "https://picsum.photos/id/3/1230/500",
-        },
-        {
-          title: "Slide 5",
-          image: "https://picsum.photos/id/4/1230/500",
-        },
-        {
-          title: "Slide 6",
-          image: "https://picsum.photos/id/5/1230/500",
-        },
-        {
-          title: "Slide 7",
-          image: "https://picsum.photos/id/6/1230/500",
-        },
-        {
-          title: "Slide 8",
-          image: "https://picsum.photos/id/7/1230/500",
-        },
-      ],
+      listings: [],
     };
+  },
+  mounted() {
+    fetch(`${API_URL}/listings/random/?number=7`)
+      .then((response) => response.json())
+      .then((result) => {
+        this.listings = result;
+      });
+  },
+  methods: {
+    getImage: function(image) {
+      return image ? image : DefaultImage;
+    },
   },
 };
 </script>
