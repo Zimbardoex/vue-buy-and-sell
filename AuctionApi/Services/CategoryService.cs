d94c74a75854571d1533b6479dc7462fb7247633
zimbardoex <zimbardoex@gmail.com>
2020-07-19 14:22:36 +1200

    Seed category table

--- AuctionApi/Services/CategoryService.cs
+++ AuctionApi/Services/CategoryService.cs
@@ -3,7 +3,8 @@ using AuctionApi.Models;
 using MongoDB.Driver;
 using System.Collections.Generic;
 using System.Linq;
-
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 
 namespace AuctionApi.Services
 {
@@ -15,8 +16,31 @@ namespace AuctionApi.Services
     {
       var client = new MongoClient(settings.ConnectionString);
       var database = client.GetDatabase(settings.DatabaseName);
-
       _categories = database.GetCollection<Category>(settings.CategoriesCollectionName);
+      seed(_categories);
+    }
+
+    private void seed(IMongoCollection<Category> categoriesCollection)
+    {
+      categoriesCollection.DeleteMany(each => true);
+      var categories = new[]
+      {
+          new BsonDocument {{ "Name", "books" }},
+          new BsonDocument {{ "Name", "clothing" }},
+          new BsonDocument {{ "Name", "computers" }},
+          new BsonDocument {{ "Name", "gaming" }},
+          new BsonDocument {{ "Name", "mobile phones" }},
+          new BsonDocument {{ "Name", "motors" }},
+          new BsonDocument {{ "Name", "instruments" }},
+          new BsonDocument {{ "Name", "pets" }},
+          new BsonDocument {{ "Name", "sports" }},
+      }.ToList();
+
+      foreach (var category in categories)
+      {
+        var modelCategory = BsonSerializer.Deserialize<Category>(category);
+        categoriesCollection.InsertOne(modelCategory);
+      }
     }
 
     public List<Category> Get() =>
