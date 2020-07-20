657f187a68119df5cbfa74956de97a3f10a9ae06
zimbardoex <zimbardoex@gmail.com>
2020-07-20 15:05:42 +1200

    Add and a display meta name for all routes

--- vue-auction/src/main.js
+++ vue-auction/src/main.js
@@ -15,6 +15,8 @@ import Buefy from "buefy";
 import "buefy/dist/buefy.css";
 import store from "./store/store";
 
+const DEFAULT_TITLE = "Vue, Buy and Sell";
+
 Vue.use(VueRouter);
 
 Vue.use(Buefy);
@@ -38,28 +40,75 @@ const loggedInRedirect = (_0, _1, next) => {
 };
 
 const routes = [
-  { path: "/", component: Home },
-  { path: "/listings/search", component: ListingsSearch },
-  { path: "/listings/:category", component: ListingsCategory },
-  { path: "/listings", component: Listings },
-  { path: "/listing", component: Listing, name: "listing" },
-  { path: "/my-listings", component: MyListings, name: "myListing" },
+  {
+    path: "/",
+    component: Home,
+    meta: {
+      title: DEFAULT_TITLE,
+    },
+  },
+  {
+    path: "/listings/search",
+    component: ListingsSearch,
+    meta: {
+      title: "Search Listings",
+    },
+  },
+  {
+    path: "/listings/:category",
+    component: ListingsCategory,
+    meta: {
+      title: "Category",
+    },
+  },
+  {
+    path: "/listings",
+    component: Listings,
+    meta: {
+      title: "All Listings",
+    },
+  },
+  {
+    path: "/listing",
+    component: Listing,
+    name: "listing",
+    meta: {
+      title: "Listing",
+    },
+  },
+  {
+    path: "/my-listings",
+    component: MyListings,
+    name: "myListing",
+    meta: {
+      title: "My listings",
+    },
+  },
   {
     path: "/create-listing",
     component: CreateListing,
     name: "create-listing",
+    meta: {
+      title: "Create a listing",
+    },
     beforeEnter: notLoggedInRedirect,
   },
   {
     path: "/signup",
     component: Signup,
     name: "signup",
+    meta: {
+      title: "Signup",
+    },
     beforeEnter: loggedInRedirect,
   },
   {
     path: "/login",
     component: Login,
     name: "login",
+    meta: {
+      title: "Login",
+    },
     beforeEnter: loggedInRedirect,
   },
 ];
@@ -68,11 +117,17 @@ const router = new VueRouter({
   routes,
 });
 
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})
+
 new Vue({
   router,
   store,
   beforeCreate() {
     this.$store.commit("initialiseStore");
+    document.title = DEFAULT_TITLE;
   },
   render: (h) => h(App),
 }).$mount("#app");
