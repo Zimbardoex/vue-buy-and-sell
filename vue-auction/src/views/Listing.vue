5948306bb028f666308f17ceda0ff084c1cb6049
zimbardoex <zimbardoex@gmail.com>
2020-09-01 10:06:24 +1200

    Add seller information section

--- vue-auction/src/views/Listing.vue
+++ vue-auction/src/views/Listing.vue
@@ -18,6 +18,32 @@
       <h3 class="title is-5">Description</h3>
       {{ listing.description }}
     </div>
+    <div class="seller" v-if="seller">
+      <h3 class="title is-5">Seller Info</h3>
+      <div class="card">
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <img
+                  :src=checkImage(seller.image)
+                  alt="Placeholder image"
+                />
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">{{ seller.name }}</p>
+              <p class="subtitle is-6">{{ seller.email }}</p>
+            </div>
+          </div>
+          <div class="content">
+              {{ seller.id }}
+            <br />
+            Member since: <time datetime="2020-1-1">11:09 PM - 1 Jan 2020</time>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -29,7 +55,8 @@ const API_URL = "http://localhost:5000/api";
 export default {
   name: "Listing",
   data: () => ({
-    listing: {},
+    listing: null,
+    seller: null,
   }),
   mounted() {
     const listing_id = this.$route.query.id;
@@ -38,6 +65,14 @@ export default {
       .then((result) => {
         this.listing = result;
         document.title = this.listing.name;
+
+        if ("userId" in result) {
+          fetch(`${API_URL}/users/${result.userId}`)
+            .then((response) => response.json())
+            .then((result) => {
+              this.seller = result;
+            });
+        }
       });
   },
   methods: {
@@ -65,6 +100,9 @@ export default {
   align-items: flex-start;
   justify-content: flex-start;
 }
+.seller {
+  margin-top: 1rem;
+}
 .image-container {
   width: 100%;
   background-color: #eee;
@@ -72,7 +110,7 @@ export default {
 .image {
   max-height: 22rem;
   max-width: 80%;
-  margin:  0 auto;
+  margin: 0 auto;
 }
 .purchasing {
   width: 100%;
@@ -80,8 +118,8 @@ export default {
   display: flex;
   justify-content: center;
 }
-.purchasing  > *{
-  margin: 0 0.5rem
+.purchasing > * {
+  margin: 0 0.5rem;
 }
 .price {
   color: black;
