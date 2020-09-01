114130e901844992d608be52c5dabcac02c29ebc
zimbardoex <zimbardoex@gmail.com>
2020-09-01 18:43:29 +1200

    Change page styling

--- vue-auction/src/views/NotFound.vue
+++ vue-auction/src/views/NotFound.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
-    <h1 class="title is-2">(404) Page NotFound</h1>
-    <img :src="SadFace" alt="Image of a sad cartoon face." />
+  <div class="container">
+    <img
+      :src="require('../assets/sadFace.png')"
+      DefaultImage
+      alt="Image of a sad cartoon face."
+      class="image"
+    />
+    <h1 class="title is-2">(404) Page not found</h1>
   </div>
 </template>
 
-<script>
-import SadFace from "../assets/sad-iace.png";
-
-export default {};
-</script>
+<style scoped>
+.container {
+  width: 100%;
+}
+.title {
+  text-align: center;
+}
+.image {
+  margin: 0 auto;
+  margin-top: 5rem;
+  margin-bottom: 2rem;
+  width: 20%;
+}
+</style>
