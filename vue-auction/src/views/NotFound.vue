2e52871dbe6206c85ce8b300585560808c00823b
zimbardoex <zimbardoex@gmail.com>
2020-09-01 17:51:14 +1200

    Add a not found view

---
+++ vue-auction/src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="title is-2">(404) Page NotFound</h1>
+    <img :src="SadFace" alt="Image of a sad cartoon face." />
+  </div>
+</template>
+
+<script>
+import SadFace from "../assets/sad-iace.png";
+
+export default {};
+</script>
